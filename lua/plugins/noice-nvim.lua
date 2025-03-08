d34db95caba5f2f9ec1f059179a6cc27c58b8a81
a.gorbunov <a.gorbunov@kt-team.de>
2025-03-08 12:25:29 +0400

    fix message

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -6,6 +6,7 @@ return {
     local utils = require "astrocore"
     return utils.extend_tbl(opts, {
       lsp = {
+        signature = { enabled = false }, -- Отключает подсказки для сигнатур функций
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -15,9 +16,9 @@ return {
       },
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
-        -- command_palette = true, -- position the cmdline and popupmenu together
+        command_palette = false, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
     })
